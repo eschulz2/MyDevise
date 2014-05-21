5e354ad15bcad7620fbd78e4d3ed926767574ea2
Eric Schulz <eschulz2@gmail.com>
2014-05-21 15:01:41 -0500

    setup gemfile for heroku, worked on user model

--- Gemfile
+++ Gemfile
@@ -12,9 +12,18 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'spring',        group: :development
 gem 'bootstrap-sass'
 gem 'devise'
+gem 'gibbon'
+# gem 'google_drive', '0.3.8'
+gem 'high_voltage'
+gem 'simple_form'
 group :development do
   gem 'better_errors'
   gem 'binding_of_caller', :platforms=>[:mri_20]
   gem 'quiet_assets'
   gem 'rails_layout'
+end
+group :production do
+  gem 'pg'
+  gem 'rails_12factor'
+  gem 'thin'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # def subscribe
+  #   # mailchimp = Gibbon::API.new(Rails.application.secrets.mailchimp_api_key)
+  #   # result = mailchimp.lists.subscribe({
+  #   #   :id => Rails.application.secrets.mailchimp_list_id,
+  #   #   :email => {:email => self.email},
+  #   #   :double_optin => false,
+  #   #   :update_existing => true,
+  #   #   :send_welcome => true
+  #   # })
+  #   # Rails.logger.info("Subscribed #{self.email} to MailChimp") if result
+  # end
 end
